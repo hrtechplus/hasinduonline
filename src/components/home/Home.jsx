4a0364fda45331f0752a958cf4e680cc5c0bd95a
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2023-12-30 18:58:15 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import './Home.css';
-import Me from '../../assets/avatar-1.svg';
-import HeaderSocials from './HeaderSocials';
-import ScrollDown from './ScrollDown';
-import Shapes from './Shapes';
+import React from "react";
+import "./Home.css";
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
 
 const Home = () => {
-    return (
-        <section className="home container" id='home'>
-            <div className="intro">
-                <img src={Me} alt="" className='home__img' width='120' />
-                <h1 className="home__name">Greg Sithole</h1>
-                <span className="home__education">I'm a Software Engineer</span>
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" width="120" />
+        <h1 className="home__name">Hasindu Rangika</h1>
+        <span className="home__education">I'm a Software Engineer</span>
 
-                <HeaderSocials />
+        <HeaderSocials />
 
-                <a href="#contact" className="btn"> Contact Me</a>
+        <a href="#contact" className="btn">
+          {" "}
+          Contact Me
+        </a>
 
-                <ScrollDown />
-            </div>
+        <ScrollDown />
+      </div>
 
-            <Shapes />
-        </section>
-    )
-}
+      <Shapes />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
