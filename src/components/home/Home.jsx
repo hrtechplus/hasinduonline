129057ea8b57fe7c3c10d9847a968d85ce3e9494
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2023-12-30 19:00:14 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="intro">
         <img src={Me} alt="" className="home__img" width="120" />
         <h1 className="home__name">Hasindu Rangika</h1>
-        <span className="home__education">I'm a Software Engineer</span>
+        <span className="home__education">I'm a Passionate Developer</span>
 
         <HeaderSocials />
 
