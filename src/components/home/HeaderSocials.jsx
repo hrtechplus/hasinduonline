cd99c2932dd47fc8f96749622b6b5cdae639c577
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2023-12-30 19:05:10 +0530

    Update HeaderSocials.jsx

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,35 +1,62 @@
-import React from 'react';
-import { FaGithub, FaFacebookF, FaLinkedinIn, FaTwitter, FaDev } from 'react-icons/fa';
-import { SiHashnode } from 'react-icons/si'
+import React from "react";
+import { FaGithub, FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const HeaderSocials = () => {
-    return (
-        <div className='home__socials'>
-            <a href='https://www.github.com/GregSithole' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaGithub />
-            </a>
+  return (
+    <div
+      className="home__socials"
+      style={{ justifyContent: "space-evenly", gap: "0" }}
+    >
+      <a
+        href="https://www.github.com/hrtechplus"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub />
+      </a>
 
-            <a href='https://hashnode.com/@gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
+      {/* <a href='https://hashnode.com/@gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
                 <SiHashnode />
-            </a>
+            </a> */}
 
-            <a href='https://dev.to/gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaDev />
-            </a>
+      {/* <a
+        href="https://dev.to/gregsithole"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaDev />
+      </a> */}
 
-            <a href='https://www.linkedin.com/in/greg-sithole-b6350558/' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaLinkedinIn />
-            </a>
+      <a
+        href="https://www.linkedin.com/in/hasinduonline/"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaLinkedinIn />
+      </a>
 
-            <a href='https://wwww.facebook.com/Greg.Sithole' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaFacebookF />
-            </a>
+      <a
+        href="https://wwww.facebook.com/hasinduonline"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaFacebookF />
+      </a>
 
-            <a href='https://www.twitter.com/GregSithole' className='home__social-link' target='__blank' rel='noreferrer'>
-                <FaTwitter />
-            </a>
-        </div>
-    );
+      <a
+        href="https://www.twitter.com/hrtechplus"
+        className="home__social-link"
+        target="__blank"
+        rel="noreferrer"
+      >
+        <FaTwitter />
+      </a>
+    </div>
+  );
 };
 
 export default HeaderSocials;
