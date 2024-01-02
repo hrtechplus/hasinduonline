bcd6c4bc4646c8eca9fd2a588f8fd209a1e9d153
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-02 16:01:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import './App.css';
+import "./App.css";
 
-import Sidebar from './components/sidebar/Sidebar';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Services from './components/services/Services';
-import Resume from './components/resume/Resume';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Blog from './components/blog/Blog';
-import Contact from './components/contact/Contact';
-import useLocalStorage from 'use-local-storage'
-
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Resume from "./components/resume/Resume";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
+import useLocalStorage from "use-local-storage";
 
 function App() {
-    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
 
-    const switchTheme = () => {
-        const newTheme = theme === 'light' ? 'dark' : 'light';
-        setTheme(newTheme);
-    }
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
 
-    return (
-        <div className="app" data-theme={theme}>
-            <Sidebar theme={theme} switchTheme={switchTheme} />
-            <main className='main'>
-                <Home />
-                <About />
-                <Services />
-                <Resume />
-                <Portfolio />
-                <Testimonials />
-                <Blog />
-                <Contact theme={theme} />
-            </main>
-        </div>
-    );
+  return (
+    <div className="app" data-theme={theme}>
+      <Sidebar theme={theme} switchTheme={switchTheme} />
+      <main className="main">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Blog />
+        <Contact theme={theme} />
+      </main>
+      <script
+        src="https://s.pageclip.co/v1/pageclip.js"
+        charset="utf-8"
+      ></script>
+    </div>
+  );
 }
 
 export default App;
