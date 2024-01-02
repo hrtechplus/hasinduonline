8c3aa9ad74cf7ccc2b211ec5c302ebbd1e056249
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-02 23:20:17 +0530

    button fixed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import "./Home.css";
 import Me from "../../assets/avatar-1.svg";
-
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 import Shapes from "./Shapes";
 import Button from "./Button";
 
 const Home = () => {
-  const backgroundImageUrl = "../../assets/imgesBg/ooorganize.svg";
   return (
-    <section
-      className="home container"
-      id="home"
-      style={{ background: `url(${backgroundImageUrl})` }}
-    >
+    <section className="home container" id="home">
       <div className="intro">
         <img src={Me} alt="" className="home__img" width="120" />
         <h1 className="home__name">Hasindu Rangika</h1>
