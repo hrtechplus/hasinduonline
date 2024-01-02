6a41ebbda05b0a1c075328a0f9ae1932fd4ab74c
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-02 15:38:04 +0530

    Create Menu.jsx

---
+++ src/components/blog/Menu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Menu = [
+  {
+    id: 1,
+    image: Dictionary,
+    title: "Sinhala-English Dictionary",
+    url: "https://dictionary.hasindu.online/",
+  },
+  {
+    id: 2,
+    image: Dictionary,
+    title: "Sinhala-English Dictionary",
+    url: "https://dictionary.hasindu.online/",
+  },
+];
+
+export default Menu;
